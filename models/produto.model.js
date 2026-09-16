95390dc3635e2dfc166b231c8c779422f64343b7
Ana Cecilia <271367@unifio.edu.br>
2026-09-16 23:31:18 +0000

    1609

--- models/produto.model.js
+++ models/produto.model.js
@@ -1,36 +1,51 @@
 class Produto {
-  static produtos = [
-    {
-      id: 1,
-      nome: "Notebook",
-      preco: 3500
-    },
-    {
-      id: 2,
-      nome: "Mouse",
-      preco: 120
+    static produtos = [
+        {
+            id: 1,
+            nome: "Notebook",
+            preco: 3500
+        },
+        {
+            id: 2,
+            nome: "Mouse",
+            preco: 120
+        },
+        {
+            id: 3,
+            nome: "Teclado",
+            preco: 200
+        },
+        {
+            id: 4,
+            nome: "Fone",
+            preco: 150
+        },
+        {
+            id: 5,
+            nome: "Monitor",
+            preco: 900
+        }
+    ];
+
+    static listar() {
+        return this.produtos;
     }
-  ];
 
-  static listar() {
-    return this.produtos;
-  }
+    static buscarPorId(id) {
+        return this.produtos.find(produto => produto.id === id);
+    }
 
-  static buscarPorId(id) {
-    return this.produtos.find(produto => produto.id === id);
-  }
+    static criar(dados) {
+        const novoProduto = {
+            id: this.produtos.length + 1,
+            nome: dados.nome,
+            preco: dados.preco
+        };
 
-  static criar(dados) {
-    const novoProduto = {
-      id: this.produtos.length + 1,
-      nome: dados.nome,
-      preco: dados.preco
-    };
+        this.produtos.push(novoProduto);
 
-    this.produtos.push(novoProduto);
-
-    return novoProduto;
-  }
+        return novoProduto;
+    }
 }
 
 module.exports = Produto;
